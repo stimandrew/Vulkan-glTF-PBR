147a8679c8e3724b5037b58e685fe86ab0061e57
stimandrew <berillo.andrey@yandex.ru>
2026-02-15 12:11:04 +0300

    Первый коммит. Никаких изменений

---
+++ base/VulkanglTFModel.h
@@ -0,0 +1,273 @@
+/**
+ * Vulkan glTF model and texture loading class based on tinyglTF (https://github.com/syoyo/tinygltf)
+ *
+ * Copyright (C) 2018-2024 by Sascha Willems - www.saschawillems.de
+ *
+ * This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
+ */
+
+#pragma once
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <fstream>
+#include <vector>
+
+#include "vulkan/vulkan.h"
+#include "VulkanDevice.hpp"
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <gli/gli.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include "basisu_transcoder.h"
+
+// ERROR is already defined in wingdi.h and collides with a define in the Draco headers
+#if defined(_WIN32) && defined(ERROR) && defined(TINYGLTF_ENABLE_DRACO) 
+#undef ERROR
+#pragma message ("ERROR constant already defined, undefining")
+#endif
+
+#define TINYGLTF_NO_STB_IMAGE_WRITE
+
+#if defined(__ANDROID__)
+#define TINYGLTF_ANDROID_LOAD_FROM_ASSETS
+#include <android/asset_manager.h>
+#endif
+
+#include "tiny_gltf.h"
+
+// Changing this value here also requires changing it in the vertex shader
+#define MAX_NUM_JOINTS 128u
+
+namespace vkglTF
+{
+	struct Node;
+
+	struct BoundingBox {
+		glm::vec3 min;
+		glm::vec3 max;
+		bool valid = false;
+		BoundingBox();
+		BoundingBox(glm::vec3 min, glm::vec3 max);
+		BoundingBox getAABB(glm::mat4 m);
+	};
+
+	struct TextureSampler {
+		VkFilter magFilter;
+		VkFilter minFilter;
+		VkSamplerAddressMode addressModeU;
+		VkSamplerAddressMode addressModeV;
+		VkSamplerAddressMode addressModeW;
+	};
+
+	struct Texture {
+		vks::VulkanDevice *device;
+		VkImage image;
+		VkImageLayout imageLayout;
+		VkDeviceMemory deviceMemory;
+		VkImageView view;
+		uint32_t width, height;
+		uint32_t mipLevels;
+		uint32_t layerCount;
+		VkDescriptorImageInfo descriptor;
+		VkSampler sampler;
+		void updateDescriptor();
+		void destroy();
+		void fromglTfImage(tinygltf::Image& gltfimage, std::string path, TextureSampler textureSampler, vks::VulkanDevice* device, VkQueue copyQueue);
+	};
+
+	struct Material {		
+		enum AlphaMode{ ALPHAMODE_OPAQUE, ALPHAMODE_MASK, ALPHAMODE_BLEND };
+		AlphaMode alphaMode = ALPHAMODE_OPAQUE;
+		float alphaCutoff = 1.0f;
+		float metallicFactor = 1.0f;
+		float roughnessFactor = 1.0f;
+		glm::vec4 baseColorFactor = glm::vec4(1.0f);
+		glm::vec4 emissiveFactor = glm::vec4(0.0f);
+		vkglTF::Texture *baseColorTexture;
+		vkglTF::Texture *metallicRoughnessTexture;
+		vkglTF::Texture *normalTexture;
+		vkglTF::Texture *occlusionTexture;
+		vkglTF::Texture *emissiveTexture;
+		bool doubleSided = false;
+		struct TexCoordSets {
+			uint8_t baseColor = 0;
+			uint8_t metallicRoughness = 0;
+			uint8_t specularGlossiness = 0;
+			uint8_t normal = 0;
+			uint8_t occlusion = 0;
+			uint8_t emissive = 0;
+		} texCoordSets;
+		struct Extension {
+			vkglTF::Texture *specularGlossinessTexture;
+			vkglTF::Texture *diffuseTexture;
+			glm::vec4 diffuseFactor = glm::vec4(1.0f);
+			glm::vec3 specularFactor = glm::vec3(0.0f);
+		} extension;
+		struct PbrWorkflows {
+			bool metallicRoughness = true;
+			bool specularGlossiness = false;
+		} pbrWorkflows;
+		VkDescriptorSet descriptorSet = VK_NULL_HANDLE;
+		int index = 0;
+		bool unlit = false;
+		float emissiveStrength = 1.0f;
+	};
+
+	struct Primitive {
+		uint32_t firstIndex;
+		uint32_t indexCount;
+		uint32_t vertexCount;
+		Material &material;
+		bool hasIndices;
+		BoundingBox bb;
+		Primitive(uint32_t firstIndex, uint32_t indexCount, uint32_t vertexCount, Material& material);
+		void setBoundingBox(glm::vec3 min, glm::vec3 max);
+	};
+
+	struct Mesh {
+		std::vector<Primitive*> primitives;
+		BoundingBox bb;
+		BoundingBox aabb;
+		glm::mat4 matrix;
+		glm::mat4 jointMatrix[MAX_NUM_JOINTS]{};
+		uint32_t jointcount{ 0 };
+		uint32_t index;
+		Mesh(glm::mat4 matrix);
+		~Mesh();
+		void setBoundingBox(glm::vec3 min, glm::vec3 max);
+	};
+
+	struct Skin {
+		std::string name;
+		Node *skeletonRoot = nullptr;
+		std::vector<glm::mat4> inverseBindMatrices;
+		std::vector<Node*> joints;
+	};
+
+	struct Node {
+		Node *parent;
+		uint32_t index;
+		std::vector<Node*> children;
+		glm::mat4 matrix;
+		std::string name;
+		Mesh *mesh;
+		Skin *skin;
+		int32_t skinIndex = -1;
+		glm::vec3 translation{};
+		glm::vec3 scale{ 1.0f };
+		glm::quat rotation{};
+		BoundingBox bvh;
+		BoundingBox aabb;
+		bool useCachedMatrix{ false };
+		glm::mat4 cachedLocalMatrix{ glm::mat4(1.0f) };
+		glm::mat4 cachedMatrix{ glm::mat4(1.0f) };
+		glm::mat4 localMatrix();
+		glm::mat4 getMatrix();
+		void update();
+		~Node();
+	};
+
+	struct AnimationChannel {
+		enum PathType { TRANSLATION, ROTATION, SCALE };
+		PathType path;
+		Node *node;
+		uint32_t samplerIndex;
+	};
+
+	struct AnimationSampler {
+		enum InterpolationType { LINEAR, STEP, CUBICSPLINE };
+		InterpolationType interpolation;
+		std::vector<float> inputs;
+		std::vector<glm::vec4> outputsVec4;
+		std::vector<float> outputs;
+		glm::vec4 cubicSplineInterpolation(size_t index, float time, uint32_t stride);
+		void translate(size_t index, float time, vkglTF::Node* node);
+		void scale(size_t index, float time, vkglTF::Node* node);
+		void rotate(size_t index, float time, vkglTF::Node* node);
+	};
+
+	struct Animation {
+		std::string name;
+		std::vector<AnimationSampler> samplers;
+		std::vector<AnimationChannel> channels;
+		float start = std::numeric_limits<float>::max();
+		float end = std::numeric_limits<float>::min();
+	};
+
+	struct Model {
+
+		vks::VulkanDevice *device;
+
+		struct Vertex {
+			glm::vec3 pos;
+			glm::vec3 normal;
+			glm::vec2 uv0;
+			glm::vec2 uv1;
+			glm::uvec4 joint0;
+			glm::vec4 weight0;
+			glm::vec4 color;
+		};
+
+		struct Vertices {
+			VkBuffer buffer = VK_NULL_HANDLE;
+			VkDeviceMemory memory;
+		} vertices;
+		struct Indices {
+			VkBuffer buffer = VK_NULL_HANDLE;
+			VkDeviceMemory memory;
+		} indices;
+
+		glm::mat4 aabb;
+
+		std::vector<Node*> nodes;
+		std::vector<Node*> linearNodes;
+
+		std::vector<Skin*> skins;
+
+		std::vector<Texture> textures;
+		std::vector<TextureSampler> textureSamplers;
+		std::vector<Material> materials;
+		std::vector<Animation> animations;
+		std::vector<std::string> extensions;
+
+		struct Dimensions {
+			glm::vec3 min = glm::vec3(FLT_MAX);
+			glm::vec3 max = glm::vec3(-FLT_MAX);
+		} dimensions;
+
+		struct LoaderInfo {
+			uint32_t* indexBuffer;
+			Vertex* vertexBuffer;
+			size_t indexPos = 0;
+			size_t vertexPos = 0;
+		};
+
+		std::string filePath;
+
+		void destroy(VkDevice device);
+		void loadNode(vkglTF::Node* parent, const tinygltf::Node& node, uint32_t nodeIndex, const tinygltf::Model& model, LoaderInfo& loaderInfo, float globalscale);
+		void getNodeProps(const tinygltf::Node& node, const tinygltf::Model& model, size_t& vertexCount, size_t& indexCount);
+		void loadSkins(tinygltf::Model& gltfModel);
+		void loadTextures(tinygltf::Model& gltfModel, vks::VulkanDevice* device, VkQueue transferQueue);
+		VkSamplerAddressMode getVkWrapMode(int32_t wrapMode);
+		VkFilter getVkFilterMode(int32_t filterMode);
+		void loadTextureSamplers(tinygltf::Model& gltfModel);
+		void loadMaterials(tinygltf::Model& gltfModel);
+		void loadAnimations(tinygltf::Model& gltfModel);
+		void loadFromFile(std::string filename, vks::VulkanDevice* device, VkQueue transferQueue, float scale = 1.0f);
+		void drawNode(Node* node, VkCommandBuffer commandBuffer);
+		void draw(VkCommandBuffer commandBuffer);
+		void calculateBoundingBox(Node* node, Node* parent);
+		void getSceneDimensions();
+		void updateAnimation(uint32_t index, float time);
+		Node* findNode(Node* parent, uint32_t index);
+		Node* nodeFromIndex(uint32_t index);
+	};
+}
